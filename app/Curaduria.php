74f40382907843d06e8857aca3c31876ec5ad55a
fernastereo <fernandoecueto@gmail.com>
2019-09-30 17:19:31 -0500

    Implementando las propiedades iniciales del modelo Curaduria

--- app/Curaduria.php
+++ app/Curaduria.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curaduria extends Model
 {
-    //
+    const CURADURIA_ACTIVA = 'ACTIVA';
+    const CURADURIA_INACTIVA = 'INACTIVA';
+
+    protected $fillable = [
+        'num_curaduria',
+        'ciudad_id',
+        'curador',
+        'idcurador',
+        'direccion',
+        'telefono',        
+        'email',
+        'fechaini',
+        'status'
+    ];
+
+    public function estaActiva(){
+        return $this->status == Curaduria::CURADURIA_ACTIVA;
+    }
 }
