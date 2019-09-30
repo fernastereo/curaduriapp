6214f5e6de08c56e6309f76140abbdfb61e5f27e
fernastereo <fernandoecueto@gmail.com>
2019-09-30 17:46:33 -0500

    Implementando las propiedades en User

--- app/User.php
+++ app/User.php
@@ -10,13 +10,16 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    const USUARIO_VERIFICADO = '1';
+    const USUARIO_NO_VERIFICADO = '0';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'verified', 'verification_token', 'admin'
     ];
 
     /**
@@ -25,7 +28,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'verification_token'
     ];
 
     /**
@@ -36,4 +39,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function esUsuarioVerificado(){
+        return $this->verified == User::USUARIO_VERIFICADO;
+    }
+
+    public static function generarVerificationToken(){
+        return str_random(40);
+    }
 }
