ecc227f151ae875ebfd06dfd33b44c0123915500
fernastereo <fernandoecueto@gmail.com>
2019-10-07 17:03:19 -0500

    Creado el seeder para ObjetoLicencia y EstadosExpedientes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CiudadsTableSeeder::class);
+        $this->call(EstadoexpedientesTableSeeder::class);
+        $this->call(ObjetolicenciasTableSeeder::class);
     }
 }
