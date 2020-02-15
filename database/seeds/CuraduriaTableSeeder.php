12ca340a0fb46f5f8feeaa7f6224d90dafd5a13e
Fernando Cueto <fernandoecueto@gmail.com>
2020-02-15 18:11:32 +0000

    Se agrego mutador y accesor al atributo name del modelo Users y se separo el seeder de Curadurias del DatabaseSeeder

--- database/seeds/CuraduriaTableSeeder.php
+++ database/seeds/CuraduriaTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use App\Curaduria;
 use Illuminate\Database\Seeder;
 
 class CuraduriaTableSeeder extends Seeder
@@ -11,6 +13,19 @@ class CuraduriaTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //Aqui llamar el factory de Curadurias, en este se crean curadurias y usuarios por cada una
+        /*
+        Ejecuto el factory Curaduria 10 veces con create y por cada uno de los registros creados 
+        mando una funcion anonima (callback) a la que le mando como parametro cada uno de los 
+        registros curaduria ($curaduria) que se van creando (para esto es el each) y dentro de esa 
+        funcion ejecuto el factory User, x veces con create y al create le mando en un array la 
+        relacion entre las dos tablas asignandole al campo relacion la variable que envié como 
+        parametro en el callback
+        */
+        factory(Curaduria::class, 20)->create()->each(function($curaduria){
+            factory(User::class, mt_rand(1, 10))->create([
+                'curaduria_id' => $curaduria->id,
+            ]);
+        });
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -38,22 +38,10 @@ class DatabaseSeeder extends Seeder
             CiudadsTableSeeder::class,
             EstadoexpedientesTableSeeder::class,
             ObjetolicenciasTableSeeder::class,
-            TipoproyectoTableSeeder::class
+            TipoproyectoTableSeeder::class,
+            CuraduriaTableSeeder::class,
         ]);
 
-        //Aqui llamar el factory de Curadurias, en este se crean curadurias y usuarios por cada una
-        /*
-        Ejecuto el factory Curaduria 10 veces con create y por cada uno de los registros creados 
-        mando una funcion anonima (callback) a la que le mando como parametro cada uno de los 
-        registros curaduria ($curaduria) que se van creando (para esto es el each) y dentro de esa 
-        funcion ejecuto el factory User, x veces con create y al create le mando en un array la 
-        relacion entre las dos tablas asignandole al campo relacion la variable que envié como 
-        parametro en el callback
-        */
-        factory(Curaduria::class, 20)->create()->each(function($curaduria){
-            factory(User::class, mt_rand(1, 10))->create([
-                'curaduria_id' => $curaduria->id,
-            ]);
-        });
+
     }
 }
