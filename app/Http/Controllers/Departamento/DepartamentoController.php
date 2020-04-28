4188f88fd8302a88e49c982bc3308c739996d25f
Fernando Cueto <fernandoecueto@gmail.com>
2020-04-28 23:30:02 +0000

    Creadas las operaciones para el controlador Departamento

--- app/Http/Controllers/Departamento/DepartamentoController.php
+++ app/Http/Controllers/Departamento/DepartamentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Departamento;
 
+use App\Departamento;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
@@ -14,28 +15,8 @@ class DepartamentoController extends ApiController
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
+        $departamentos = Departamento::all();
+        return $this->showAll($departamentos);
     }
 
     /**
@@ -46,40 +27,7 @@ class DepartamentoController extends ApiController
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $departamento = Departamento::findOrFail($id);
+        return $this->showOne($departamento);
     }
 }
