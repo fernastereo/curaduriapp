89c35419a11235f6cba23bc54e5723b96c8163e2
Fernando Cueto <fernandoecueto@gmail.com>
2020-06-02 22:58:40 +0000

    Se creo el método upload para cargue de archivos al bucket

--- app/Http/Controllers/Solicitud/SolicitudController.php
+++ app/Http/Controllers/Solicitud/SolicitudController.php
@@ -150,4 +150,37 @@ class SolicitudController extends ApiController
         $data = $solicitud::where('id', $solicitud->id)->with('solicitante')->with('solicitudanexos')->get();
         return $this->showAll($data);
     }
+
+    /**
+     * Upload a file on temporary folder in the S3 bucket
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function upload(Request $request)
+    {
+        
+        try{
+            $folder = $request->has('folder') ? $request->folder : null;
+            $archivo = $request->has('archivo') ? $request->file('archivo') : null;
+            
+            /*Para enviar a S3: 
+            Crear usuario en AWS y asignarles las policies hacia el bucket (copiar de otro)
+            instalar esto: composer require league/flysystem-aws-s3-v3
+            En config/filesystem declarar el bucket
+            En .env colocar las credenciales
+            */
+            $storagePath = Storage::disk('solicitudfiles')->put('solicituds_files/' . $folder, $archivo, 'public');
+            
+            //Guardo la url completa para acceder al archivo dentro del bucket en la variable $url
+            $url = Storage::url($storagePath);
+            $data['success'] = 'success';
+            $data['path'] = $url;
+        }catch(\Exception $e){
+            return $this->errorResponse('warning,Something Went Wrong!' . $e->getMessage(), 500);
+        }   
+
+        return $data;
+
+    }
 }
