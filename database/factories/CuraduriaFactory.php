76187532361e13c7d326d2e97521218a7df10239
vagrant <vagrant@curaduriapp>
2020-07-13 23:21:58 +0000

    Actualizada factory de Curadurias

--- database/factories/CuraduriaFactory.php
+++ database/factories/CuraduriaFactory.php
@@ -9,15 +9,15 @@ use Faker\Generator as Faker;
 $factory->define(Curaduria::class, function (Faker $faker) {
     return [
         'ciudad_id'     => Ciudad::all()->random(),
-        'numero'        => $faker->randomElement([1, 2, 3]),
+        'numero'        => $faker->randomElement([1, 2, 3, 4, 5]),
         'curador'       => $faker->name,
-        'idcurador'     => $faker->randomNumber(8, false),
+        'idcurador'     => $faker->randomNumber(12, false),
         'direccion'     => $faker->streetAddress,
         'telefono'      => $faker->phoneNumber,
         'email'         => $faker->freeEmail,
         'web'           => 'http://www.' . $faker->domainName,
         'logo'          => $faker->imageUrl($width = 250, $height = 250, 'business'),
-        'fechaini'      =>$faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now', $timezone = null),
+        'fechaini'      => $faker->dateTimeBetween($startDate = '-2 years', $endDate = 'now', $timezone = null),
         'estado'        => $faker->randomElement([Curaduria::CURADURIA_ACTIVA, Curaduria::CURADURIA_INACTIVA]),
     ];
 });
